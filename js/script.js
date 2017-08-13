9c3eaa39d9f2efe7503c12a183450d4396caaf70
Bartek Kozera <bartosz.kozera@infor.com>
2017-08-13 11:36:24 +0200

    adding browser-sync and exercises

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// player properties
+// dane gracza
 
 console.log(headers);
 
-// example 
+// przyklad na wyciagniecie imion graczy
 
 function getPlayerName(player) {
     return player.PLAYER;
@@ -11,4 +11,52 @@ function getPlayerName(player) {
 console.log(data.map(getPlayerName));
 
 
+// ZADANIA
 
+// zadanie 1
+// posortuj zawodnikow wg ilosci zbiorek ("REB"). Marcin Gortat powinien byc na 12 miejscu (index 11)!
+
+
+
+
+// zadanie 2
+// pokaz tylko tych graczy ktorzy rozegrali wszystkie mecze w sezonie ("GP"). Wszystkich meczy bylo 82.
+
+
+
+
+
+// zadanie 3
+// wyciagnij tylko najwazniejsze dane o kazdym koszykarzu. Wyjsciowa tablica powinna zwierac obiekty z imieniem i nazwiskiem gracza (jako "name") i jego druzyne (jako "team):
+/*
+  {
+    "name": "Andrew Wiggins",
+    "team": "MIN"
+  },
+  {
+    "name": "Karl-Anthony Towns",
+    "team": "MIN"
+  },
+...
+]
+*/
+
+
+
+// zadanie 4
+// Posortuj graczy wg sumarycznej liczby minut ktore grali w calym sezonie. 
+// Uzyj do tego liczby meczy ktore grali ("GP") i sredniej ilosci minut w kazdym meczu ("MIN")
+// pokaz ich imiona i nazwiska i sumaryczna ilosci minut: ["Andrew Wiggins, 3050.4", "Karl-Anthony Towns, 3034"...]
+
+
+
+
+
+// zadanie 5
+// Pokaz imiona i nazwiska wszystkich koszykarzy z druzyn ("TEAM") ktore zaczynaja sie na "C". 
+
+
+
+
+// zadanie 6
+// pokaz graczy wszystkich graczy z druzyny ("TEAM") Chicago BUlls (skrot "CHI"). Posortuj ich po ilosci asyst ("AST") ktore zdobyli.
